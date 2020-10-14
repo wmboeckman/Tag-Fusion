085b9f2ed677e30670a0bd0f4de46e40b9e17612
William Boeckman <wmboeckman@gmail.com>
2020-10-13 20:02:27 -0500

    - Added TODO notes to main python entrypoint. - Directed entrypoint directly to cli.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import app.interface.cli as cli
 
-def main():
-    pass
-
+# TODO:
+# - Build rudimentary tag generator (no extensive customization, simplistic design.)
+# - Develop boilerplate cli app to interface with the tag generator pipeline.
+# - Expand on the generator application to provide customization options (and maybe a node-based pipeline?)
+# - Build rudimentary UI interface
 
 if __name__ == '__main__':
-    main()
+    cli.cli()
