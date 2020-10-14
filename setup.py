faa356d118043a2177d9560d39d6c6fc1da640d2
William Boeckman <wmboeckman@gmail.com>
2020-10-13 19:58:24 -0500

    Click cli interface setup.

--- setup.py
+++ setup.py
@@ -16,5 +16,11 @@ setup(
     author_email='wmboeckman@gmail.com',
     url='https://github.com/wmboeckman/Tag-Fusion',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    py_modules=['main'],
+    install_requires=['Click'],
+    entry_points='''
+        [console_scripts]
+        main=main:cli
+    ''',
 )
